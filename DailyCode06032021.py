fa4ccbc5712695aaf0c203743f23f42bc492e59d
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-03 06:20:59 -0700

    (One Line) To the Power of...
    Daily Code for 06/03/2021

---
+++ DailyCode06032021.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------
+# #	Daily Code	06/03/2021
+#   "(One Line) To the Power of..." Lesson from edabit.com
+#   Coded by: Banehowl
+# ---------------------------------------------------------------------
+
+# Create a function that takes a base number and an exponent number and returns the calculation.
+
+# calculate_exponent(5, 5) -> 3125
+# calculate_exponent(10, 10) -> 10000000000
+# calculate_exponent(3, 3) -> 27
+
+def calculate_exponent(x, y):
+    return x ** y
+
+print calculate_exponent(5, 5)
+print calculate_exponent(10, 10)
+print calculate_exponent(3, 3)
