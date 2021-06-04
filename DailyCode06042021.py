cecca1873f5682ee641fe142f34ca5f28ee9c868
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-04 06:17:35 -0700

    (One Line) Age to Days
    Daily Code for 06/04/2021

---
+++ DailyCode06042021.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------
+# #	Daily Code	06/04/2021
+#   "(One Line) Age to Days" Lesson from edabit.com
+#   Coded by: Banehowl
+# ---------------------------------------------------------------------
+
+# Create a function that takes the age in years and returns the age in days.
+
+# calc_age(65) -> 23725
+# calc_age(0) -> 0
+# calc_age(20) -> 7300
+
+def calc_age(num):
+    return num * 365
+
+print calc_age(65)
+print calc_age(0)
+print calc_age(20)
