3ccd2344fe940c3bcee0446f0c78f91b9a0840b4
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-05 06:42:55 -0700

    (One Line) Boolean to String Conversion
    Daily Code for 06/05/2021

---
+++ DailyCode06052021.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------
+# #	Daily Code	06/05/2021
+#   "(One Line) Boolean to String Conversion" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------------------------------------
+
+# Create a function that takes a boolean variable flag and returns it as a string.
+
+def bool_to_string(flag):
+    return str(flag)
+
+print bool_to_string(True)
+print bool_to_string(False)
