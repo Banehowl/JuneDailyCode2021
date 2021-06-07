b22c3538d10b3a474e886d617ab0026355a36f3a
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-07 06:22:11 -0700

    (One Line) Proper Modulo Operator
    Daily Code for 06/07/2021

---
+++ DailyCode06072021.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------
+# #	Daily Code	06/07/2021
+#   "(One Line) Proper Modulo Operator" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------------------------------
+
+# Create a function which returns the Modulo of the two given numbers.
+
+# mod(-13, 64) -> 51
+# mod(50, 25) -> 0
+# mod(-6, 3) -> 0
+
+def mod(x, y):
+    return x % y
+
+print mod(-13, 64)
+print mod(50, 25)
+print mod(-6, 3)
