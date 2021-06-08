2e5da534886aeab79e969c70f5e0998c543404f4
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-08 06:22:46 -0700

    (One Line) Return the Subtration
    Daily Code for 06/08/2021

---
+++ DailyCode06082021.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------------------
+# #	Daily Code	06/07/2021
+#   "(One Line) Return the Subtration" Lesson from edabit.com
+#   Coded by: Banehowl
+# -------------------------------------------------------------
+
+# Create a function that takes two numbers as arguments and return the subtraction.
+
+def addition(x, y):
+    return x - y
+
+print addition(3, 2)
+print addition(-3, -6)
+print addition(7, 3)
