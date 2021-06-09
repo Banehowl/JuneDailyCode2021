95b6e478a84fc756d17e89d435e2b935fe892e12
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-09 06:16:06 -0700

    (One Line) Return the Multiplication
    Daily Code for 06/09/2021

---
+++ DailyCode06092021.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------
+# #	Daily Code	06/07/2021
+#   "(One Line) Return the Multiplication" Lesson from edabit.com
+#   Coded by: Banehowl
+# -----------------------------------------------------------------
+
+# Create a function that takes two numbers as arguments and return the solution.
+
+def addition(x, y):
+    return x * y
+
+print addition(3, 2)
+print addition(-3, -6)
+print addition(7, 3)
