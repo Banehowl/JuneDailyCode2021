390502939abe81b38b2150d338715b4987296cbe
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-15 06:18:22 -0700

    (One Line) Find the Perimeter of a Rectangle
    Daily Code for 06/15/2021

---
+++ DailyCode06152021.py
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------------
+# #	Daily Code	06/14/2021
+#   "(One Line) Find the Perimeter of a Rectangle" Lesson from edabit.com
+#   Coded by: Banehowl
+# -------------------------------------------------------------------------
+
+# Create a function that takes length and width and finds the perimeter of a rectangle.
+
+# find_perimeter(6, 7) -> 26
+# find_perimeter(20, 10) -> 60
+# find_perimeter(2, 9) -> 22
+
+def find_perimeter(length, width):
+    return (length + width) * 2
+
+print find_perimeter(6, 7)
+print find_perimeter(20, 10)
+print find_perimeter(2, 9) 
