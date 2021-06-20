c66532cc4a570cb06a16b7e3740903ca1b6a268e
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-20 06:14:42 -0700

    Happy Father's Day Check
    Daily Code for 06/20/2021

---
+++ DailyCode06202021.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------
+# #	Daily Code	06/20/2021
+#   "Happy Father's Day" Lesson from edabit.com
+#   Coded by: Banehowl
+# -----------------------------------------------
+
+def fathersDayCheck(txt):
+    if txt == "yes":
+        return "Happy Father's Day!"
+    else:
+        return "Nope"
+
+print fathersDayCheck("yes")
+print fathersDayCheck("no")
