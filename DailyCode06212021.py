670cefec0ef56b26dfa20283cae8a57ff0fd7ff2
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-21 06:23:49 -0700

    Prime Day Check
    Daily Code for 06/21/2021

---
+++ DailyCode06212021.py
@@ -0,0 +1,14 @@
+# --------------------------------------
+# #	Daily Code	06/21/2021
+#   "Prime Day" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------
+
+def primeDayCheck(txt):
+    if txt == "yes":
+        return "Happy Prime Day!"
+    else:
+        return "Nope"
+
+print primeDayCheck("yes")
+print primeDayCheck("no")
