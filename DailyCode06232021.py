2e5e9ec21028ee074e60eaba5dab1f8c8bcc94d8
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-23 06:15:20 -0700

    Prime Day Over
    Daily Code for 06/23/2021

---
+++ DailyCode06232021.py
@@ -0,0 +1,14 @@
+# ----------------------------------------
+# #	Daily Code	06/23/2021
+#   "Prime Day Over" Lesson from edabit.com
+#   Coded by: Banehowl
+# ----------------------------------------
+
+def primeDayCheck(txt):
+    if txt == "yes":
+        return "Welp...actually it's done =("
+    else:
+        return "Nope"
+
+print primeDayCheck("yes")
+print primeDayCheck("no")
