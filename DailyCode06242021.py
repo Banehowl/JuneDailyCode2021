021f20502b1ac2f4c8e13c5c31872fd5b88483ad
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-24 06:13:27 -0700

    Interview Day
    Daily Code for 06/24/2021

---
+++ DailyCode06242021.py
@@ -0,0 +1,14 @@
+# ------------------------------------------
+# #	Daily Code	06/24/2021
+#   "Interview Day" Lesson from edabit.com
+#   Coded by: Banehowl
+# ------------------------------------------
+
+def interviewCheck(txt):
+    if txt == "yes":
+        return "Good luck man. You got this!"
+    else:
+        return "No really, you got this! Let's get it!"
+
+print interviewCheck("yes")
+print interviewCheck("no")
