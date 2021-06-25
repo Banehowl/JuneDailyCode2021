74c6ab27c13aaaf9edde612027463b6ab292b549
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-25 06:12:38 -0700

    Interview Day (Part 2)
    Daily Code for 06/25/2021

---
+++ DailyCode06252021.py
@@ -0,0 +1,14 @@
+# ---------------------------------------------------
+# #	Daily Code	06/25/2021
+#   "Interview Day (Part 2)" Lesson from edabit.com
+#   Coded by: Banehowl
+# ---------------------------------------------------
+
+def interviewCheck(txt):
+    if txt == "yes":
+        return "This is it man! You got it!"
+    else:
+        return "Absolute trash process for real..."
+
+print interviewCheck("yes")
+print interviewCheck("no")
