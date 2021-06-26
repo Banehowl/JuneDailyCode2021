5167b985300ba31c5f47ab808bf9ae3889c9a89e
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-26 06:45:09 -0700

    Code Check
    Daily Code for 06/26/2021

---
+++ DailyCode06262021.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------
+# #	Daily Code	06/26/2021
+#   "Final Coding Days" Lesson from edabit.com
+#   Coded by: Banehowl
+# ----------------------------------------------
+
+def codeCheck(txt):
+    if txt == "yes":
+        return "It was fun! Learned a lot"
+    else:
+        return "Might come back to it again"
+
+print codeCheck("yes")
+print codeCheck("no")
