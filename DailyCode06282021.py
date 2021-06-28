08da453a6f708b2a6f48268bede9dbb4b073c642
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-28 06:14:33 -0700

    New Hobby of RC Drifting (Part 2)
    Daily Code for 06/28/2021

---
+++ DailyCode06282021.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------------------
+# #	Daily Code	06/28/2021
+#   "New Hobby of RC Drifting (Part 2" Lesson from edabit.com
+#   Coded by: Banehowl
+# -------------------------------------------------------------
+
+def rcCheck(txt):
+    if txt == "yes":
+        return "Looking into the Sakura d5s v2"
+    else:
+        return "Aww but it's cool."
+
+print rcCheck("yes")
+print rcCheck("no")
