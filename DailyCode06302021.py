6317aee06156e6ebd676ac47c6ce69e0a0863b2e
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-06-30 06:15:15 -0700

    Final Daily Code
    Final Daily Code for 06/30/2021. I may come back to it, but running into time constraints. Peace! Had fun learning!

---
+++ DailyCode06302021.py
@@ -0,0 +1,14 @@
+# -------------------------------------------
+# #	Daily Code	06/30/2021
+#   "Final Daily Code" Lesson from Banehowl
+#   Coded by: Banehowl
+# -------------------------------------------
+
+def finalCode(txt):
+    if txt == "yes":
+        return "Learned a lot. It was fun!"
+    else:
+        return "There's always tomorrow!"
+
+print finalCode("yes")
+print finalCode("no")
